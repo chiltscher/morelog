686029d4e316959ed6c99e7f4c5ad4e49ee40ffe
Christian Hiltscher <hiltscher.christian@gmail.com>
2017-03-25 14:26:29 +0100

    added new server function and some first styling

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const colors = require('colors');
+var express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const fs = require('fs');
 const os = require('os');
-const eol = os.EOL;
-const http = require('http');
+const eol = '\r\n';
 
 //  create a new instance of morelog with a specific name
 //  for example the name off the application using morelog
 var Morelog = function(prefix, color) {
 
+    // some specifications
     this.m_prefix = prefix.toUpperCase();
     this.m_colors = {};
     this.m_colors["print"] = color;
@@ -16,14 +19,25 @@ var Morelog = function(prefix, color) {
     this.m_colors["info"] = 'yellow';
     this.m_colors["debug"] = 'yellow';
 
+    // modes
     this.m_debugMode = this._isDebugMode();
+
+    // files
+    this.m_path = '.mrlgs';
     this.m_logDir = this._setupDir();
     this.m_extension = '.mrlg';
+    this.m_logFiles = this._getAvailableLogFiles();
 
+    // errors
+    this.m_errors = [];
+
+    // server
+    this.m_app = express();
     this.m_server = undefined;
     this.m_port = undefined;
 }
-
+// outpout methods
+//==============================================================================
 Morelog.prototype.print = function(text, preventLog) {
 
     var date = this._getTime();
@@ -74,6 +88,8 @@ Morelog.prototype.debug = function(text) {
     console.log(colors[color](data));
 }
 
+// helpers and functions to check some things
+//==============================================================================
 Morelog.prototype._isDebugMode = function() {
 
     var args = process.argv;
@@ -85,44 +101,66 @@ Morelog.prototype._isDebugMode = function() {
 }
 
 Morelog.prototype._getTime = function() {
-    return new Date().toLocaleTimeString() + ' ';
+    return new Date().toLocaleTimeString();
 }
 
 Morelog.prototype._getDate = function() {
-    return new Date().toLocaleDateString() + ' ';
+    return new Date().toLocaleDateString();
 }
 
+// file system functions
+//==============================================================================
 Morelog.prototype._setupDir = function(){
-    var path = '.logs';
-    if(!fs.existsSync(path))
-        fs.mkdirSync(path);
-    return path
+    var PATH = this.m_path;
+    if(!fs.existsSync(PATH))
+        fs.mkdirSync(PATH);
+    return PATH
 }
+
+Morelog.prototype._getAvailableLogFiles = function(){
+
+    this.m_logFiles = [];
+    var PATH = this.m_path;
+    var dir = fs.readdirSync(PATH);
+    var that = this;
+
+    dir.forEach(function(file){
+        if(path.extname(file) === that.m_extension)
+            that.m_logFiles.push(file);
+    });
+};
 
 Morelog.prototype.log = function(data){
 
     var filename = this.m_logDir + '/' + this._getDate() + this.m_extension;
     fs.appendFileSync(filename, data + eol);
-
+    this._getAvailableLogFiles();
 }
 
+// server that provides every log file
+//==============================================================================
 Morelog.prototype.startLogServer = function(port) {
 
     var that = this;
+    var app = this.m_app;
 
     if (this.m_server)
         return false;
-
     if (!port)
         this.error("Can not start Log-Server. Port required!");
 
     this.m_port = port;
-    this.m_server = http.createServer(function(req, res) {
-        that.__provideLog(req, res)
-    });
-    this.__serverEvents();
-    this.m_server.listen(port)
 
+    app.set('view engine', 'ejs');
+    app.use('/views', express.static('views'));
+    app.use('/lib', express.static('lib'));
+
+    app.get('/', function(req, res) {
+        that._getAvailableLogFiles();
+        res.render('index', {logFiles: that.m_logFiles});
+    });
+
+    this.m_server = app.listen(port);
 
     return true;
 }
@@ -132,25 +170,10 @@ Morelog.prototype.__serverEvents = function() {
     var that = this;
     var server = this.m_server;
 
-    server.on('connection', function(request, response) {
-        that.info("Log-Server accessed");
-    });
-
-    server.on('listening', function() {
-        that.info("Logfile provided @port " + that.m_port)
-    });
+    // server.on('connection', function(request, response) {
+    //     that.info("Log-Server accessed");
+    // });
 }
-
-Morelog.prototype.__provideLog = function(req, res) {
-
-    var log = fs.readFileSync(this.m_file);
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end(log);
-
-};
 
 Morelog.prototype.closeLogServer = function() {
 
@@ -163,11 +186,15 @@ Morelog.prototype.closeLogServer = function() {
 
 }
 
+
+// Morelog error // in process
+//==============================================================================
 Morelog.Error = function(name, origin, solution) {
 
     this.m_name = name;
     this.m_origin = origin;
     this.m_solution = solution;
+    this.m_code = code;
 
     Morelog.m_errors.push(this);
 }
